11c0887507dc7f7ef555f05b23eb8af4fb49edc1
mkruglov <mrmarkussss@gmail.com>
2023-11-08 18:42:34 +0200

    Refactor: added use String Format to xpath construction

--- herokuapp/src/main/java/org/example/Tables.java
+++ herokuapp/src/main/java/org/example/Tables.java
@@ -5,8 +5,8 @@ import static com.codeborne.selenide.Selenide.$x;
 public class Tables {
 
     public String getCellValue(int tableNumber, int tableRow, int tableCell){
-        String xpath = "//table[@id='table" + tableNumber +
-                "']//tr[" + tableRow + "]//td[" + tableCell +"]";
+        String xpath = String.format("//table[@id='table%d']//tr[%d]//td[%d]",
+                tableNumber, tableRow, tableCell);
         return $x(xpath).getText();
     }
 }
